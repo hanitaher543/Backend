0f78521ce80f662a3db8d8a10789e89768272d63
hanitaher0106 <hani.taher2000@gmail.com>
2024-06-30 19:38:43 +0100

    create post and get request with using then/cach & try/catch

--- server.js
+++ server.js
@@ -56,10 +56,72 @@ app.post('/create', async (req, res) =>{
 })
 
 
-app.get( '/getAll', ()=>{
-    console.log('Get Work');
+//getAll using .then .catch
+app.get( '/getAll', (req, res)=>{
+    
+User.find()
 
-});
+.then(
+    (users) =>{
+        res.send(users)
+        
+    }
+)
+
+.catch(
+    (err) =>{
+        res.send(err);
+    }
+)
+} );
+
+//getAll using try .. catch
+app.get('/getAllUsers',async (req, res)=>{
+  
+    try{
+        users = await User.find()
+
+        res.send(users)
+
+    } catch(error){
+        res.send(error)
+
+    }
+})
+
+//get user by id
+app.get('/getbyid/:id', (req, res)=>{
+
+    myid = req.params.id;
+
+    User.findOne({ _id : myid })
+
+    .then(
+        (user) =>{
+            res.send(user)
+        }
+    )
+    .catch(
+        (err) =>{
+            res.send(err)
+        }
+    )
+})
+
+//get user by id with using try & catch 
+app.get(('/getbyiduser/:id'), async (req, res)=>{
+
+    try{
+        myid = req.params.id;
+        user = await User.findOne({ _id : myid})
+
+        res.send(user)
+
+
+    }catch(error){
+        res.send(error)
+    }
+})
 
 
 app.put('/update', ()=>{
@@ -72,6 +134,11 @@ app.delete( '/delete', ()=>{
 });
 
 
+
+
+
+
+//Run server !!
 app.listen( 3000, () => {
     console.log('server work')
 });

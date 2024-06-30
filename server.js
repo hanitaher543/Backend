69d0754cfdcab558417fb9423b41226513b59251
hanitaher0106 <hani.taher2000@gmail.com>
2024-06-30 12:07:27 +0100

    create request and test it with postman

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 
 
-
+//Hérite de toutes les fonctions disponibles dans Express.
 const app  = express();
 
+
+//Create request : GET, POST, PUT and DELETE
+app.post( '/add', () =>{
+   console.log('Add work');
+} );
+
+app.get( '/getAll', ()=>{
+    console.log('Get Work');
+
+});
+
+
+app.put('/update', ()=>{
+    console.log('Update Work');
+});
+
+app.delete( '/delete', ()=>{
+    console.log('Delete Work');
+
+});
 
 
 app.listen( 3000, () => {
